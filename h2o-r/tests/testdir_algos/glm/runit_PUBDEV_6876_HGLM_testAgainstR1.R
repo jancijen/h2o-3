562e8d2e9c761e4cb6e5f93a7764e620d8346f1f
wendycwong <wendycwong@yahoo.com>
2019-09-26 13:21:00 -0700

    Finished r model.

--- h2o-r/tests/testdir_algos/glm/runit_PUBDEV_6876_HGLM_testAgainstR1.R
+++ h2o-r/tests/testdir_algos/glm/runit_PUBDEV_6876_HGLM_testAgainstR1.R
@@ -1,59 +1,28 @@
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../../scripts/h2o-r-test-setup.R")
 library(MASS)
+library(hglm)
 ##
 # Copy example from HGLM package and compare results from our implementation.
 ##
 
 test.HGLMData1 <- function() {
+  browser()
   set.seed(123)
   n.clus <- 5 # number of clusters
   n.per.clus <- 20 # Number of points within each cluster
   sigma2_u <- 0.2  # variance of random effect
   sigma2_e <- 1 # residual_deviance
-  n <- n.clus
-
-
-
-  print("Read in prostate data.")
-  h2o.data = h2o.uploadFile(locate("smalldata/prostate/prostate_complete.csv.zip"), destination_frame="h2o.data")    
-  R.data = as.data.frame(as.matrix(h2o.data))
-  print("Testing for family: Negative Binomial")
-  print("Set variables for h2o.")
-  myY = "GLEASON"
-  myX = c("ID","AGE","RACE","CAPSULE","DCAPS","PSA","VOL","DPROS")
-  print("Define formula for R")
-  R.formula = (R.data[,"GLEASON"]~.) 
-  ##################  Test with fixed theta and log link
-  print("Create models with canonical link: LOG and fixed theta")
-  browser()
-
-  	print("Compare model deviances for link function log")
-
-
-  	h2onegbinomialLog <- h2o.glm(x=myX, y=myY, training_frame=h2o.data, family="negbinomial", link="log",alpha=0.5,
-  	                                     lambda=0, nfolds=0, theta=1.0)
-    rnegbinomialLog <- glm(formula=R.formula, data=R.data[,2:9], family=negative.binomial(link=log, theta=1.0), na.action=na.omit)
-    compareModels(h2onegbinomialLog, rnegbinomialLog)
-  	print("Compare model deviances for link function IDENTITY")
-
-  	print("Create models with link: IDENTITY")
-  	h2onegbinomialIdentity <- h2o.glm(x=myX, y=myY, training_frame=h2o.data, family="negbinomial", link="log",alpha=0.5,
-  	                             lambda=0, nfolds=0, theta=1.0)  rpoissonIdentity <- glm(formula=R.formula, data=R.data[,2:9], family=negative.binomial(link=identity), na.action=na.omit)
-    compareModels(h2onegbinomialIdentity, rpoissonIdentity)
-    }
-
-compareModels <- function(h2oModel, rModel){
-  # compare deviance
-  h2oDeviance = h2oModel@model$training_metrics@metrics$residual_deviance / h2oModel@model$training_metrics@metrics$null_deviance
-  rDeviance = deviance(rModel)  / h2oModel@model$training_metrics@metrics$null_deviance
-  difference = rDeviance - h2oDeviance
-  if (difference > 0.01) {
-    print(cat("Deviance in H2O: ", h2oDeviance))
-    print(cat("Deviance in R: ", rDeviance))
-
-  }
-}
+  n <- n.clus*n.per.clus
+    X <-matrix(1,n,1)
+  Z <- diag(n.clus)%x%rep(1,n.per.clus)
+  a <- rnorm(n.clus, 0, sqrt(sigma2_u))
+  e <- rnorm(n, 0, sqrt(sigma2_e))
+  mu <- 0
+  y <- mu+Z%*%a+e
+  lmm <- hglm(y=y, X=X, Z=Z)
+  summary(lmm)
+ }
 
 doTest("Comparison of H2O to R with HGLM 1", test.HGLMData1)
 
