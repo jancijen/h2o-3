7347dcda984ad5d81a2cb8ff9ee95ebb0623f2c5
wendycwong <wendycwong@yahoo.com>
2019-09-26 19:07:16 -0700

    Still fixing runit.

--- h2o-r/tests/testdir_algos/glm/runit_PUBDEV_6876_HGLM_testAgainstR1.R
+++ h2o-r/tests/testdir_algos/glm/runit_PUBDEV_6876_HGLM_testAgainstR1.R
@@ -8,20 +8,15 @@ library(hglm)
 
 test.HGLMData1 <- function() {
   browser()
-  set.seed(123)
-  n.clus <- 5 # number of clusters
-  n.per.clus <- 20 # Number of points within each cluster
-  sigma2_u <- 0.2  # variance of random effect
-  sigma2_e <- 1 # residual_deviance
-  n <- n.clus*n.per.clus
-    X <-matrix(1,n,1)
-  Z <- diag(n.clus)%x%rep(1,n.per.clus)
-  a <- rnorm(n.clus, 0, sqrt(sigma2_u))
-  e <- rnorm(n, 0, sqrt(sigma2_e))
-  mu <- 0
-  y <- mu+Z%*%a+e
-  lmm <- hglm(y=y, X=X, Z=Z)
-  summary(lmm)
+  data(semiconductor)
+  m11 <- hglm(fixed = y ~ x1 + x3 + x5 + x6,
+              random = ~ 1|Device,
+              family = Gamma(link = log),
+              disp = ~ x2 + x3, data = semiconductor)
+  summary(m11)
+  plot(m11, cex = .6, pch = 1,
+       cex.axis = 1/.6, cex.lab = 1/.6,
+       cex.main = 1/.6, mar = c(3, 4.5, 0, 1.5))
  }
 
 doTest("Comparison of H2O to R with HGLM 1", test.HGLMData1)
